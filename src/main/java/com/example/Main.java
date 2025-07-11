553aa9c3c6f4ebc2556fb6291fe8feb707855d4e
yuyuhee00 <yuyuhee00@naver.com>
2025-07-11 17:27:50 +0900

    1단계

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,77 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import static java.lang.System.out;
+
+
+public class Main {
+
+    private static void doJob() {
+
+//        out.println("종료");
+//        out.println("==게시판 앱==");
+        out.println("doJob()");
+    }
+
+    private static void intInput() {
+
+        Scanner scanner = new Scanner(System.in);
+        int input = 1;
+
+        out.println("== 게시판 앱 ==");
+
+        while (input != 0) {
+
+            out.printf("입력 : ");
+
+            try {
+                input = scanner.nextInt();
+                out.printf("입력된 문자열 : %d\n", input);
+
+                doJob();
+
+//                 scanner.nextInt();
+            } catch (InputMismatchException e) {
+                out.println("입력 에러..");
+                scanner.nextInt();
+            }
         }
+        scanner.close();
+    }
+
+    private static void stringInput() {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        out.println("== 게시판 앱 ==");
+
+        while (! input.equals("종료")) {
+
+            out.printf("명령 : ");
+
+            try {
+                input = scanner.nextLine();
+                out.printf("입력된 문자열 : %s\n", input);
+
+                doJob();
+
+                // scanner.nextLine();
+            } catch (InputMismatchException e) {
+                out.println("입력 에러..");
+                scanner.nextLine();
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+
+        Main.intInput();
+//        Main.stringInput();
+
     }
 }
